e90493479de8a5fc6be08bf7adc6b6a58849f49a
Thomas Wagner <thomas.wagner@pa-systems.de>
2021-06-24 13:03:57 +0200

    fix get all

--- uarray.c
+++ uarray.c
@@ -137,8 +137,7 @@ int uarray_get_used(uarray_st* ua, void** items) {
     pthread_rwlock_rdlock(&ua->lock);
     for (int ii = 0; ii < ua->max_len; ii++) {
         if (ua->entries[ii].used) {
-            void** item = items + res;
-            *item = &ua->entries[ii].item;
+            items[res] = ua->entries[ii].item;
             res++;
         }
     }
